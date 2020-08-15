b1b07b1979e36c3ad6e0e008f5c75258de07b8e9
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-08-15 17:27:16 +0100

    install browserAsync and update gulpfile

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 // const autoprefixer = require('gulp-autoprefixer');
 // const cleanCSS = require('gulp-clean-css');
 
@@ -12,12 +13,19 @@ function style() {
     // .pipe(autoprefixer())
     // 3. where do I save the compiled css
     .pipe(gulp.dest('./css'))
+    // 4. stream changes to all browser
+    .pipe(browserSync.stream());
 
 }
 
 // watch for changes automatically
 function watch() {
+    browserSync.init({
+        baseDir: './'
+    });
      gulp.watch('./scss/main.scss', style);
+     gulp.watch('../index.html').on('change', browserSync.reload);
+
 }
 
 
