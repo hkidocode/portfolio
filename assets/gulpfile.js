9a7f6e5d83a3b2e7d702a0d4ac62478d18022338
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-08-24 15:07:45 +0100

    fix some errors in gulpjs file

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,42 +1,28 @@
-const gulp = require('gulp');
+
+
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-// const autoprefixer = require('gulp-autoprefixer');
-// const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
-// compile scss into css
-function style() {
-    // 1. where is my scss file
-    return gulp.src('./scss/main.scss')
-    // 2. pass that file through sass compiler
-    .pipe(sass())
-    // .pipe(autoprefixer())
-    // 3. where do I save the compiled css
-    .pipe(gulp.dest('./css'))
-    // 4. stream changes to all browser
-    .pipe(browserSync.stream());
+
+sass.compiler = require('node-sass');
+
+
+function styles() {
+    return src('scss/**/*.scss')
+        .pipe(sass({ outputStyle: "expanded" }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(dest('css'));
 
 }
 
-// watch for changes automatically
-function watch() {
-    browserSync.init({
-        baseDir: './'
-    });
-     gulp.watch('./scss/main.scss', style);
-     gulp.watch('../index.html').on('change', browserSync.reload);
+function watcher() {
+    return watch('scss/**/*.scss', styles);
 
 }
 
-
-// function minifying() {
-//     return gulp.src('./css/style.css')
-//     .pipe(cleanCSS({compatibility: 'ie8'}))
-//     .pipe(gulp.dest('dist'));
-// }
+exports.watch = watcher;
 
 
 
-exports.style = style;
-exports.watch = watch;
-// exports.cleanCSS = cleanCSS;
+
