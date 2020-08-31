182c926365cb822a1b2c873bc9129a469111919b
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-08-31 19:21:41 +0100

    style modal in about section

---
+++ assets/js/modal.js
@@ -0,0 +1,46 @@
+// // start value of progress bar
+// let start = 0;
+// // run progress bar until end value reached 
+// const frame = () => {
+//     start++;
+//     if(start === 85) {
+//         clearInterval(frame);
+//     } else {
+//         const counter = document.querySelector('.progress-skill__counter');
+//         counter.textContent = `${start}`;
+//     }
+// }
+
+// const stopPoint = () => {
+//     const stop = document.querySelector('.progress-skill__stop');
+//     stop.style.display = 'block';
+// }
+
+// setInterval(frame, 50);
+// setTimeout(stopPoint, 3000)
+
+const quickView = document.querySelector('.btn-wrap__view');
+const modal = document.querySelector('.modal');
+const timesButton = document.querySelector('.fa-times');
+
+
+// show modal if quick view button is clicked
+const showModal = () => {
+    modal.style.display = 'block';
+}
+quickView.addEventListener('click', showModal);
+
+
+// hide modal if times button is clicked
+const hideModal = () => {
+    modal.style.display = 'none';
+}
+timesButton.addEventListener('click', hideModal);
+
+// hide modal if window is clicked
+const outsideModal = (e) => {
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+window.addEventListener('click', outsideModal);
