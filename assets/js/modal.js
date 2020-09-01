2e4e795fd7e5c02222a57b7b60ebc8cc44498bbe
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-09-01 15:04:57 +0100

    Revert "try something"
    This reverts commit ad46131e27405ae26a72f7dff2809dc4d276624c.

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,4 +1,24 @@
-// select all elements we need to show and hide modal
+// // start value of progress bar
+// let start = 0;
+// // run progress bar until end value reached 
+// const frame = () => {
+//     start++;
+//     if(start === 85) {
+//         clearInterval(frame);
+//     } else {
+//         const counter = document.querySelector('.progress-skill__counter');
+//         counter.textContent = `${start}`;
+//     }
+// }
+
+// const stopPoint = () => {
+//     const stop = document.querySelector('.progress-skill__stop');
+//     stop.style.display = 'block';
+// }
+
+// setInterval(frame, 50);
+// setTimeout(stopPoint, 3000)
+
 const quickView = document.querySelector('.btn-wrap__view');
 const modal = document.querySelector('.modal');
 const timesButton = document.querySelector('.fa-times');
@@ -19,9 +39,8 @@ timesButton.addEventListener('click', hideModal);
 
 // hide modal if window is clicked
 const outsideModal = (e) => {
-    if (e.target == modal) {
+    if(e.target == modal) {
         modal.style.display = 'none';
     }
 }
 window.addEventListener('click', outsideModal);
-
